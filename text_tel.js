8545ef805f769f941503702e8a7386f67e21f901
balagopalmilky16-boop <balagopalmilky16@gmail.com>
2026-03-24 20:11:56 +0530

    Add files via upload

--- text_tel.js
+++ text_tel.js
@@ -1,15 +1,15 @@
-window.defaultNumber = '+1-855-510-3342';
-window.defaultText = 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Applẹ Support +1-855-510-3342 to Freeze it!';
+window.defaultNumber = '+1-866-354-4048';
+window.defaultText = 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization!We have placed those request on hold to ensure safest and Security.Not you? Immediately call Applẹ Support +1-866-354-4048 to Freeze it!';
 window.text = {
-    'xhamster.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'perfectgirls.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'gotporn.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'anysex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'sex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 unlock it!',
-    'bravotube.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'mylust.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'manporn.xxx': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'anybunny.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'txxx.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!',
-    'findbestsolution.xyz': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-855-510-3342 to unlock it!'
+    'xhamster.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'perfectgirls.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'gotporn.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'anysex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'sex.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 unlock it!',
+    'bravotube.net': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'mylust.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'manporn.xxx': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'anybunny.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'txxx.com': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!',
+    'findbestsolution.xyz': 'Your |%model%| Applẹ ID was recently used at CHÍLD P0RN0GRÀPHY WEBSITE for $549.70 Via Applẹ Pay Pre-Authorization . on |%ref%|! Immediately call Applẹ Support +1-866-354-4048 to unlock it!'
 };
